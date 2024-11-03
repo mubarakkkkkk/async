bd3514f853c59f6ed8c8d63393a2e68af09ed493
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-03 23:24:30 +0100

    task commit

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-setTimeout(() => {
-    console.log("1. first one")
-}, 1000);
-setTimeout(() => {
-    console.log("2. secound option")
-}, 500);
-setTimeout(() => {
-    console.log("3. third one")
-},2000);
+function task1(callback) {
+    setTimeout(() => {
+        console.log("1. first one")
+        callback();
+    }, 1000);
+}
+function task2(callback) {
+    setTimeout(() => {
+        console.log("2. secound option")
+        callback();
+    }, 500);
+}
+function task3(callback) {
+    setTimeout(() => {
+        console.log("3. third one")
+        callback();
+    },2000);
+}
+task1(() => {
+    task2(() => {
+        task3(() => {
+            
+        })
+    })
+})
