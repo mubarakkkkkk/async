f67cc2094962298d0d23bdaceb48f5783aa18359
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-03 23:12:42 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    console.log("1. first one")
+}, 1000);
+setTimeout(() => {
+    console.log("2. secound option")
+}, 500);
+setTimeout(() => {
+    console.log("3. third one")
+},2000);
